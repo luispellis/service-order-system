caa131a744ec75e23e258dd17e129c1ad6ab1695
luispellis <luisfelipe_pellis@hotmail.com>
2026-03-12 22:43:29 -0300

    chore: initial spring boot project setup

---
+++ src/main/java/com/luisdev/serviceorder/ServiceOrderSystemApplication.java
@@ -0,0 +1,13 @@
+package com.luisdev.serviceorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceOrderSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceOrderSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luisdev/serviceorder/ServiceOrderSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luisdev.serviceorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceOrderSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
