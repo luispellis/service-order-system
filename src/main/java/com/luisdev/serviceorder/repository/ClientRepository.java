4c11cdf2a75fe6dc4c36b18cda11523485bc24a3
luispellis <luisfelipe_pellis@hotmail.com>
2026-03-13 22:00:42 -0300

    feat: implement client service layer

--- src/main/java/com/luisdev/serviceorder/repository/ClientRepository.java
+++ src/main/java/com/luisdev/serviceorder/repository/ClientRepository.java
@@ -3,7 +3,10 @@ package com.luisdev.serviceorder.repository;
 import com.luisdev.serviceorder.entity.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 // JpaRepository<Client, Long> = Spring Data JPA gera automaticamente save(), findAll(), FindById(), DeleteById(), count(), existsById()
 public interface ClientRepository extends JpaRepository<Client, Long> {
 
+    List<Client> id(Long id);
 }
---
+++ src/main/java/com/luisdev/serviceorder/service/ClientService.java
@@ -0,0 +1,37 @@
+package com.luisdev.serviceorder.service;
+
+import com.luisdev.serviceorder.entity.Client;
+import com.luisdev.serviceorder.repository.ClientRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+
+// @Service = Indica que essa classe pertence à camada de negócio
+@Service
+public class ClientService {
+
+    // Injeção de dependência, o Spring Framework injeta automaticamente o repository
+    private final ClientRepository clientRepository;
+
+    public ClientService(ClientRepository clientRepository) {
+        this.clientRepository = clientRepository;
+    }
+
+    public Client createClient(Client client){
+        return clientRepository.save(client);
+    }
+
+    public List<Client> getAllClient(){
+        return clientRepository.findAll();
+    }
+
+    public Client getClientById(Long id){
+        return clientRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+    }
+
+    public void deleteClient(Long id){
+        clientRepository.deleteById(id);
+    }
+}
